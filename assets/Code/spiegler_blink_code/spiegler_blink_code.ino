bbd5c8a22b5fa3f1c8ef036ffedaf559dced7230
26spiegb-sudo <26spiegb@charlottelatin.org>
2026-03-07 15:59:36 -0500

    Update spiegler_blink_code.ino

--- assets/Code/spiegler_blink_code/spiegler_blink_code.ino
+++ assets/Code/spiegler_blink_code/spiegler_blink_code.ino
@@ -1,15 +1,26 @@
 #include <MCUFRIEND_kbv.h>
 #include <Adafruit_GFX.h>
 
-MCUFRIEND-kbv tft;
-
+MCUFRIEND_kbv tft;
 
 void setup() {
-  Serial.begin(9600);
-
-  uint16_t ID = tft.readID();
-  tft.begin("LCD ID: 0x");
-  tft.setRotation(ID, HEX);   
+    uint16_t ID = tft.readID();
+    tft.begin(ID);
+    tft.setRotation(1);
 }
 
+void loop() {
+    tft.fillScreen(0xF800);
+    tft.setTextColor(0xFFFF);
+    tft.setTextSize(4);
+    tft.setCursor(90, 110);
+    tft.print("RED");
+    delay(2000);
 
+    tft.fillScreen(0x001F);
+    tft.setTextColor(0xFFFF);
+    tft.setTextSize(4);
+    tft.setCursor(75, 110);
+    tft.print("BLUE");
+    delay(2000);
+}
